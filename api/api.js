6c77b0df41ea338093bfc79352f81042d76db0d3
Almera <rapzx60@gmail.com>
2026-05-06 22:05:56 +0700

    Create api.js with Telegram configuration
    Add Telegram configuration with token and chat ID

---
+++ api/api.js
@@ -0,0 +1,9 @@
+// api/api.js
+const TELEGRAM_CONFIG = {
+    TOKEN: '8787799672:AAFlXyKsh1SjDO3cjjpw9PGd35X4EcUzh-A',
+    CHAT_ID: '7314158034'
+};
+
+// Opsional: Jika ingin lebih aman dari skrip luar, 
+// kita bisa membungkusnya agar tidak mudah dimodifikasi di console.
+Object.freeze(TELEGRAM_CONFIG);
